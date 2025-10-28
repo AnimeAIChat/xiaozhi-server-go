fd8516f8a767f3a1275fc7c779e00762a0e1e926
kalicyh <34980061+kalicyh@users.noreply.github.com>
2025-10-29 02:01:35 +0800

    ✨ feat(constants): 添加TTS类型常量以支持多种文本转语音服务 ✨ feat(vad): 实现VAD接口和基本的VAD管理功能 ✨ feat(message): 添加消息类型和状态常量以支持消息处理

---
+++ constants/constants.go
@@ -0,0 +1,28 @@
+package constants
+
+const (
+	VadTypeSileroVad = "silero_vad"
+	VadTypeWebRTCVad = "webrtc_vad"
+)
+
+const (
+	AsrTypeFunAsr = "funasr"
+	AsrTypeDoubao = "doubao"
+)
+
+const (
+	LlmTypeOpenai  = "openai"
+	LlmTypeOllama  = "ollama"
+	LlmTypeEinoLLM = "eino_llm"
+	LlmTypeEino    = "eino"
+)
+
+const (
+	TtsTypeDoubao      = "doubao"
+	TtsTypeDoubaoWS    = "doubao_ws"
+	TtsTypeCosyvoice   = "cosyvoice"
+	TtsTypeEdge        = "edge"
+	TtsTypeEdgeOffline = "edge_offline"
+	TtsTypeXiaozhi     = "xiaozhi"
+	TtsTypeOpenAI      = "openai"
+)
